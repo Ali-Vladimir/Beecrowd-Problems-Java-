a1a0ab5e8525a29709e065748a45640c8ce5f34a
Ali-Vladimir <alivladimirc@gmail.com>
2026-03-18 16:28:48 -0600

    Simple Calculate

---
+++ p1010.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p1010{
+    public static void main(String[] args) {
+        Salary object=new Salary();
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n",object.salary());
+    }
+}
+class Salary{
+    Scanner a_teclado=new Scanner(System.in);
+    double salary(){
+        a_teclado.nextLine();
+        return a_teclado.nextInt()*a_teclado.nextDouble()+a_teclado.nextInt()*a_teclado.nextDouble();
+    }
+}
